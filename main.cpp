33d8f9bb4e0b7d6796115084e43a245810d302e6
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-11-22 18:51:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,20 +2,19 @@
 using namespace std;
 
 int main() {
-   int n=0;
-    int f= 1, f1=1;
-    cin >> n;
-    if(n>=2) {
-        cout << f << endl;
-        cout << f1 << endl;
-        for (int i = 1; i < n - 1; i++) {
-            int temp = f;
-            f += f1;
-            f1 = temp;
-            cout << f << endl;
-        }
-    }else{
-        cout << "errore"<<endl;
+    int n=0,i,primoN=1,secondoN=1,terzoN;
+    do{
+        cin>>n;
+        if(n<2)
+            cout<<"errore";
+    }while(n<2);
+    cout<<primoN<<endl;
+    cout <<secondoN<<endl;
+    for(i=2;i<n;i++) {
+        terzoN = primoN + secondoN;
+        primoN = secondoN;
+        secondoN = terzoN;
+        cout << terzoN << endl;
     }
    return 0;
 }
